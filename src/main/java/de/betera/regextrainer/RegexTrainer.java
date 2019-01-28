589477947cff8a8c73cd44faa6348047844563be
Michael Bessert <michael.bessert@eisenmann.com>
2019-01-28 12:20:15 +0100

    initial

---
+++ src/main/java/de/betera/regextrainer/RegexTrainer.java
@@ -0,0 +1,19 @@
+package de.betera.regextrainer;
+
+import javax.swing.JFrame;
+
+public class RegexTrainer {
+
+	private JFrame frame;
+	
+	public RegexTrainer() {
+		
+		frame = new JFrame();
+		
+	}
+	
+	public static void main(String[] args) {
+		new RegexTrainer();
+	}
+	
+}
